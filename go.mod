5c26df43a222ae59db956acdc89c6631104324e7
frozen <wancsc@163.com>
2019-06-27 23:41:26 +0800

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module blog-api
+
+go 1.12
+
+require (
+	github.com/freezeChen/studio-library v0.0.11
+	github.com/gin-gonic/gin v1.3.0
+)
---
+++ model/arcicle.go
@@ -0,0 +1,10 @@
+package model
+
+type Article struct {
+	Id       int64
+	Title    string
+	Detail   string
+	Language string
+	Author   string
+	Time     string
+}
